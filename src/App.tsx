03cd671e42979a174e061f4273889b7910201d60
Malte Bjoerklund <maltebjoerklund@gmail.com>
2024-12-21 20:36:04 +0100

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Sorteringsalgoritmer</h1>;
+}
+
+export default App;
---
+++ src/algorithmManager.js
@@ -0,0 +1,14 @@
+class AlgorithmManager {
+  create(algorithm, array) {
+    // Return a new instance of given algorithm with given array
+    switch (algorithm) {
+      case "sort1":
+        break;
+      case "sort2":
+        break;
+
+      default:
+        return new Error("Unknown Algorithm");
+    }
+  }
+}
