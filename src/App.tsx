e238e165c586457d6805d5c59029e1dd1c151507
Malte Bjoerklund <maltebjoerklund@gmail.com>
2024-12-26 03:39:21 +0100

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <h1>Sorteringsalgoritmer</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1> Sortering Algoritmer </h1>} />
+    </Routes>
+  );
 }
 
 export default App;
