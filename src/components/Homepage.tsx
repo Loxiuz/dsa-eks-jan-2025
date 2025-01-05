fda87d5b8dc64e25b0c0d0420f86ec7a55b2c288
Malte Bjoerklund <maltebjoerklund@gmail.com>
2025-01-05 20:40:29 +0100

    Changed title

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
   return (
     <div>
-      <h1>Sorteringsalgoritmer</h1>
+      <h1>Algoritmevisualisering</h1>
       <Link to="/patience_sort_visualizer">
         <button id="patienceSortButton">Patience Sort Algorithm</button>
       </Link>
